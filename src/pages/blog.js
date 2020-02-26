d7b77d52746c6e76d9de94bdd4efac0c968ebc28
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-26 00:01:17 -0800

    navbar
    nav bar is new test

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Link} from 'gatsby';
 
 const blog = () => {
   return (
     <div>
+          <nav>
+    <Link to = "/">Home</Link>
+    </nav>
       hello from blog page
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import {Link} from 'gatsby';
 
-export default () => <div>Hello world!</div>
+export default () => <div>
+    <nav>
+    <Link to = "/blog/">Blog</Link>
+    </nav>
+    Hello world!
+    </div>
